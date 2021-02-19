ee98aad30a851da1b5449eb27c24a53b4b94ff64
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-02-19 17:57:44 -0600

    Add basic User entity

--- src/main/java/com/pjcraig/entity/User.java
+++ src/main/java/com/pjcraig/entity/User.java
@@ -1,2 +1,127 @@
-package com.pjcraig.entity;public class User {
+package com.pjcraig.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import java.util.Objects;
+import javax.persistence.*;
+
+/**
+ * The type User.
+ *
+ * @author pjcraig
+ */
+@Entity(name="User")
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    private String email;
+    private String password;
+    private String nickname;
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param email    the email
+     * @param password the password
+     * @param nickname the nickname
+     */
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Gets nickname.
+     *
+     * @return the nickname
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * Sets nickname.
+     *
+     * @param nickname the nickname
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && email.equals(user.email) && password.equals(user.password) && nickname.equals(user.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, nickname);
+    }
 }
