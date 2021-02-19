d8a6725af6732e9a40c39764ecb6dcb8999ab3b1
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-02-19 16:54:46 -0600

    Add cleandb.sql for better JUnit testing

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,2 @@
+DELETE FROM user;
+INSERT INTO user VALUES (1,'jsmitty@example.com','password','J. Smitty'),(2,'jdoe@example.com','password1234','Kat'),(3,'almarks@example.com','password4321','Marketh');
