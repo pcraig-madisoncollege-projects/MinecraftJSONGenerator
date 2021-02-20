00535c2bbcb1e1dc1a55f4430ee2ae39c3069dd2
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-02-19 19:52:50 -0600

    Add test class for CommandDao

---
+++ src/test/java/com/pjcraig/persistence/CommandDaoTest.java
@@ -0,0 +1,90 @@
+package com.pjcraig.persistence;
+
+import com.pjcraig.entity.Command;
+import com.pjcraig.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandDaoTest {
+
+    CommandDao dao;
+
+    /**
+     * Initializes the dao and testing database before any test is run.
+     */
+    @BeforeEach
+    void setUp() {
+        dao = new CommandDao();
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
+
+    /**
+     * Verifies retrieving commands by id from database.
+     */
+    @Test
+    void getByIdSuccess() {
+        Command expectedCommand = new Command("/tellraw @a [{\"text\":\"This is a test\",\"color\":\"green\"}]",
+                3, true);
+        expectedCommand.setId(2);
+        Command retrievedCommand = dao.getById(2);
+        assertNotNull(retrievedCommand);
+        assertEquals(expectedCommand, retrievedCommand);
+    }
+
+    /**
+     * Verifies success of updating commands in database.
+     */
+    @Test
+    void saveOrUpdateSuccess() {
+        int id = 3;
+        Command commandToUpdate = dao.getById(id);
+        String newCommand = "/tellraw @a [\"Hello!\"]";
+        String initialCommand = commandToUpdate.getCommand();
+        commandToUpdate.setCommand(newCommand);
+        dao.saveOrUpdate(commandToUpdate);
+        Command retrievedCommand = dao.getById(id);
+        assertNotEquals(initialCommand, retrievedCommand.getCommand());
+        assertEquals(newCommand, retrievedCommand.getCommand());
+    }
+
+    /**
+     * Verifies success of command insertion into the database.
+     */
+    @Test
+    void insertSuccess() {
+        Command newCommand = new Command("/tellraw @a [\"Why, hello there!\"]",
+                1, true);
+        int id = dao.insert(newCommand);
+        newCommand.setId(id);
+        assertNotEquals(1, id);
+        Command insertedCommand = dao.getById(id);
+        assertEquals(newCommand, insertedCommand);
+    }
+
+    /**
+     * Verifies success of command deletion from the database.
+     */
+    @Test
+    void deleteSuccess() {
+        int id = 2;
+        Command command = dao.getById(id);
+        assertNotNull(command);
+        dao.delete(command);
+        assertNull(dao.getById(id));
+    }
+
+    /**
+     * Verifies successful retrieval of all commands from the database.
+     */
+    @Test
+    void getAllSuccess() {
+        List<Command> commands = dao.getAll();
+        assertEquals(3, commands.size());
+    }
+}
