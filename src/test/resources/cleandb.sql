3900b59bf987bf00644f12e5c6060adea0022200
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-02 15:13:54 -0600

    Add SQL to reset auto increment counter to 1

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,4 +1,6 @@
 DELETE FROM command;
 DELETE FROM user;
+ALTER TABLE user AUTO_INCREMENT = 1;
+ALTER TABLE command AUTO_INCREMENT = 1;
 INSERT INTO user VALUES (1,'jsmitty@example.com','password','J. Smitty'),(2,'jdoe@example.com','password1234','Kat'),(3,'almarks@example.com','password4321','Marketh');
 INSERT INTO command VALUES (1,'/tellraw @a [\"Hello, there\"]',1,0),(2,'/tellraw @a [{\"text\":\"This is a test\",\"color\":\"green\"}]',3,1),(3,'/title @a title [{\"text\":\"Hello!\"}]',2,0);
