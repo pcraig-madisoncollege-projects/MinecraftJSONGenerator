0b2a295b64160fbc729726fe793279b1fd969471
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-07 17:26:45 -0600

    Update cleandb sql to match structure of database design

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -4,6 +4,6 @@ DELETE FROM `role`;
 ALTER TABLE `role` AUTO_INCREMENT = 1;
 ALTER TABLE `user` AUTO_INCREMENT = 1;
 ALTER TABLE `command` AUTO_INCREMENT = 1;
-INSERT INTO `role` VALUES (1,'admin');
-INSERT INTO `user` VALUES (1,'jsmitty@example.com','password','J. Smitty',1),(2,'jdoe@example.com','password1234','Kat',null),(3,'almarks@example.com','password4321','Marketh',null);
+INSERT INTO `role` VALUES (1,'admin', 'jsmitty@example.com', 1);
+INSERT INTO `user` VALUES (1,'jsmitty@example.com','password','J. Smitty'),(2,'jdoe@example.com','password1234','Kat'),(3,'almarks@example.com','password4321','Marketh');
 INSERT INTO `command` VALUES (1,'/tellraw @a [\"Hello, there\"]',1,0),(2,'/tellraw @a [{\"text\":\"This is a test\",\"color\":\"green\"}]',3,1),(3,'/title @a title [{\"text\":\"Hello!\"}]',2,0);
