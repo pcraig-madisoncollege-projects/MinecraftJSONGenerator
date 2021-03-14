c1d63b026ac3c15c522a89ff2e1a38c5583abe48
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-14 16:23:33 -0500

    Add Minecraft Profile entity that represents a player's in-game Minecraft profile

---
+++ src/main/java/com/pjcraig/entity/MCProfile.java
@@ -0,0 +1,32 @@
+package com.pjcraig.entity;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * This class stores a Minecraft player profile, consisting of a unique name and a universal unique id (uuid).
+ * @author pjcraig
+ */
+public class MCProfile {
+
+	@SerializedName("name")
+	private String name;
+
+	@SerializedName("id")
+	private String id;
+
+	/**
+	 * Gets the unique player name as seen in-game.
+	 * @return The String name.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Gets the uuid of the player profile.
+	 * @return The String uuid.
+	 */
+	public String getId() {
+		return id;
+	}
+}
