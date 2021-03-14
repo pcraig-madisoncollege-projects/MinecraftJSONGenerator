297dfe6ea9ef120de05c7d6814b8ab259c36c933
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-14 16:32:50 -0500

    Add constructors and setters to make valid JavaBean entity

--- src/main/java/com/pjcraig/entity/MinecraftProfile.java
+++ src/main/java/com/pjcraig/entity/MinecraftProfile.java
@@ -2,6 +2,8 @@ package com.pjcraig.entity;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 /**
  * This class stores a Minecraft player profile, consisting of a unique name and a universal unique id (uuid).
  * @author pjcraig
@@ -13,6 +15,23 @@ public class MinecraftProfile {
 
 	@SerializedName("id")
 	private String id;
+
+	/**
+	 * Constructs an empty Minecraft profile.
+	 */
+	public MinecraftProfile() {
+
+	}
+
+	/**
+	 * Constructs Minecraft profile consisting of a name and uuid.
+	 * @param name The name of the profile.
+	 * @param id The String uuid of the profile.
+	 */
+	public MinecraftProfile(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
 
 	/**
 	 * Gets the unique player name as seen in-game.
@@ -28,5 +47,34 @@ public class MinecraftProfile {
 	 */
 	public String getId() {
 		return id;
+	}
+
+	/**
+	 * Sets the unique player name as seen in-game.
+	 * @param name The String name.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Sets the uuid of the player profile.
+	 * @param id The String uuid.
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		MinecraftProfile profile = (MinecraftProfile) o;
+		return Objects.equals(getName(), profile.getName()) && getId().equals(profile.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getName(), getId());
 	}
 }
