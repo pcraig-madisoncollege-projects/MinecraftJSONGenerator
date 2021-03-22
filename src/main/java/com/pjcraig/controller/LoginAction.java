60e240c8048dbb2c3e7b3c93fca50be84e5d0c2d
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-22 15:50:46 -0500

    Fix user attribute from servlet context to http session

--- src/main/java/com/pjcraig/controller/LoginAction.java
+++ src/main/java/com/pjcraig/controller/LoginAction.java
@@ -6,12 +6,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -44,8 +44,9 @@ public class LoginAction extends HttpServlet {
         if (users.size() == 1) {
             User user = users.get(0);
 
-            ServletContext session = getServletContext();
+            HttpSession session = request.getSession();
             session.setAttribute("user", user);
+            logger.info("Saved user login to session attribute.");
         }
 
         logger.info("Logged in {} user with a role of {}", email, role);
