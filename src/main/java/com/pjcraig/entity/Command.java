b3b4815624da3fad5686aa2e768d7c5d350d3322
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-22 17:16:09 -0500

    Add fields for command name, group, and date modified

--- src/main/java/com/pjcraig/entity/Command.java
+++ src/main/java/com/pjcraig/entity/Command.java
@@ -3,6 +3,7 @@ package com.pjcraig.entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Objects;
 
 /**
@@ -17,6 +18,11 @@ public class Command {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    private String name;
+    private String group;
+    @Column(name="date_modified")
+    private LocalDate dateModified;
     private String command;
     private boolean shared;
 
@@ -124,18 +130,73 @@ public class Command {
         this.shared = shared;
     }
 
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets group.
+     *
+     * @return the group
+     */
+    public String getGroup() {
+        return group;
+    }
+
+    /**
+     * Sets group.
+     *
+     * @param group the group
+     */
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    /**
+     * Gets date modified.
+     *
+     * @return the date modified
+     */
+    public LocalDate getDateModified() {
+        return dateModified;
+    }
+
+    /**
+     * Sets date modified.
+     *
+     * @param dateModified the date modified
+     */
+    public void setDateModified(LocalDate dateModified) {
+        this.dateModified = dateModified;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Command command = (Command) o;
-        return getId() == command.getId() && getOwner().equals(command.getOwner())
-                && isShared() == command.isShared()
-                && getCommand().equals(command.getCommand());
+        Command command1 = (Command) o;
+        return getId() == command1.getId() && isShared() == command1.isShared() && getName().equals(command1.getName())
+                && getGroup().equals(command1.getGroup()) && getDateModified().equals(command1.getDateModified())
+                && getCommand().equals(command1.getCommand());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getCommand(), getOwner(), isShared());
+        return Objects.hash(getId(), getName(), getGroup(), getDateModified(), getCommand(), isShared());
     }
 }
