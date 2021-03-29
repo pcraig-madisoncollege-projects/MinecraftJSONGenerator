282aca89dd3d8a3e5f99295cddbf30982a524423
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-03-28 21:11:47 -0500

    Add endpoint service class for retrieving command data

--- src/main/java/com/pjcraig/controller/CommandService.java
+++ src/main/java/com/pjcraig/controller/CommandService.java
@@ -4,28 +4,57 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.pjcraig.entity.Command;
 import com.pjcraig.persistence.GenericDao;
+import com.pjcraig.util.QueryParameterLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class handles accessing the user RESTful API that is part of the Minecraft JSON Generator application.
  * @author pjcraig
  */
 @Path("/users")
-public class UserService {
+public class CommandService implements QueryParameterLoader {
     Logger logger = LogManager.getLogger();
 
     @GET
     @Produces("application/json")
-    public Response getUsers() {
+    public Response getCommands(@Context UriInfo uriInfo) {
+        Map<String, String> queryParams = getParameterMap(uriInfo.getQueryParameters());
+
         GenericDao commandDao = new GenericDao(Command.class);
-        List<Command> commands = commandDao.getByPropertyEqual("shared", true);
+        List<Command> commands = new ArrayList<>();
+
+        String parameterId = queryParams.get("id");
+        // Verify that id parameter exists
+        if (parameterId != null) {
+            try {
+                int id = Integer.parseInt(parameterId);
+
+                Command command = (Command) commandDao.getById(id);
+                // Verify that the command can be accessed
+                if (command != null && command.isShared()) {
+                    commands.add(command);
+                }
+            } catch (NumberFormatException exception) {
+                logger.info("Invalid id {} provided when searching for a command.", parameterId);
+            } catch (Exception exception) {
+                logger.error("Unknown exception occurred while retrieving command by id.", exception);
+            }
+        } else {
+            commands = commandDao.getByPropertyEqual("shared", true);
+        }
 
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(commands);
