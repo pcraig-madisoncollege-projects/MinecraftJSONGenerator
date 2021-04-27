3978dd815a1942973342385b2185aba6ed6138ac
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-04-27 13:56:25 -0500

    Clean up Element class and include additional functions and properties for ease of use

--- src/main/webapp/js/Element.js
+++ src/main/webapp/js/Element.js
@@ -6,6 +6,10 @@ class Element {
         "red", "light purple", "yellow", "reset"
     ];
 
+    static selectors = [
+        "@a", "@p", "@r", "@s"
+    ];
+
     // TODO: consider options for supporting hover and click events due to large variability per option
     static hoverEvents = [
         "none", "show text", "show item", "show entity"
@@ -14,20 +18,95 @@ class Element {
         "none", "open url", "open file", "run command", "suggest command", "change page", "copy to clipboard"
     ];
 
-    constructor(color) {
+    constructor(color, bold, italic, underlined, strikethrough, obfuscated) {
         this._color = color;
+        this._bold = bold;
+        this._italic = italic;
+        this._underlined = underlined;
+        this._strikethrough = strikethrough;
+        this._obfuscated = obfuscated;
     }
+
+    // GETTERS
 
     get color() {
         return this._color;
     }
 
+    get bold() {
+        return this._bold;
+    }
+
+    get italic() {
+        return this._italic;
+    }
+
+    get underlined() {
+        return this._underlined;
+    }
+
+    get strikethrough() {
+        return this._strikethrough;
+    }
+
+    get obfuscated() {
+        return this._obfuscated;
+    }
+
+    // SETTERS
+
     set color(color) {
         this._color = color;
     }
 
-    // Converts the generic element to a JSON representation for Minecraft
+    set bold(bold) {
+        this._bold = bold;
+    }
+
+    set italic(italic) {
+        this._italic = italic;
+    }
+
+    set underlined(underlined) {
+        this._underlined = underlined;
+    }
+
+    set strikethrough(strikethrough) {
+        this._strikethrough = strikethrough;
+    }
+
+    set obfuscated(obfuscated) {
+        this.obfuscated = obfuscated;
+    }
+
+    /*
+        Generates square brackets around target selector tags if they are missing.
+    */
+    static cleanSelectorTags(tags) {
+        if (tags != "") {
+            if (!tags.startsWith("[")) {
+                tags = "[" + tags;
+            }
+            if (!tags.endsWith("]")) {
+                tags += "]";
+            }
+        }
+
+        return tags;
+    }
+
+    /*
+        Converts the generic element to a JSON representation for Minecraft
+    */
     asJSON() {
-        return {};
+        let object = {
+            "color": this._color,
+            "bold": this._bold,
+            "italic": this._italic,
+            "underlined": this._underlined,
+            "strikethrough": this._strikethrough,
+            "obfuscated": this._obfuscated
+        };
+        return object;
     }
 }
