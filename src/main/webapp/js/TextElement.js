7cc5ce602846f24dcd7af248c3797ec2b3a7b6ca
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-04-26 23:34:50 -0500

    Add parameters to constructor so that text, color, and bold properties can be handled

--- src/main/webapp/js/TextElement.js
+++ src/main/webapp/js/TextElement.js
@@ -1,22 +1,32 @@
 class TextElement extends Element {
-    constructor() {
-        super();
-        this._text = "";
+    constructor(text, color, bold) {
+        super(color);
+        this._text = text;
+        this._bold = bold;
     }
 
     get text() {
         return this._text;
     }
 
+    get bold() {
+        return this._bold;
+    }
+
     set text(text) {
         this._text = text;
+    }
+
+    set bold(bold) {
+        this._bold = bold;
     }
 
     // Converts the text element to a JSON representation for Minecraft
     asJSON() {
         let object = {
             "text": this._text,
-            "color": this._color
+            "color": this._color,
+            "bold": this._bold
         };
 
         return object;
