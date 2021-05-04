4d9fb9a2c2871145fc2fb6d6147ce1dd9033bfe5
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-05-03 22:43:20 -0500

    Add command loading capabilities

--- src/main/webapp/js/generate.js
+++ src/main/webapp/js/generate.js
@@ -172,6 +172,8 @@ const addTextElement = () => {
     listItem.appendChild(deleteButton);
 
     appendElement(listItem);
+
+    return listItem;
 }
 
 /*
@@ -195,6 +197,8 @@ const addSelectorElement = () => {
     listItem.appendChild(deleteButton);
 
     appendElement(listItem);
+
+    return listItem;
 }
 
 /*
@@ -221,6 +225,8 @@ const addScoreElement = () => {
     listItem.appendChild(deleteButton);
 
     appendElement(listItem);
+
+    return listItem;
 }
 
 /*
@@ -250,6 +256,8 @@ const addNbtElement = () => {
     listItem.appendChild(deleteButton);
 
     appendElement(listItem);
+
+    return listItem;
 }
 
 /*
@@ -270,6 +278,8 @@ const addKeybindElement = () => {
     listItem.appendChild(deleteButton);
 
     appendElement(listItem);
+
+    return listItem;
 }
 
 /*
@@ -290,6 +300,8 @@ const addTranslateElement = () => {
     listItem.appendChild(deleteButton);
 
     appendElement(listItem);
+
+    return listItem;
 }
 
 /*
@@ -458,6 +470,237 @@ const confirmSave = () => {
 }
 
 /*
+    Opens the load confirmation form dialog.
+*/
+const openLoadForm = () => {
+    let loadForm = document.querySelector("#loadForm");
+    let feedback = document.querySelector("#loadFeedback");
+
+    feedback.textContent = "";
+    loadForm.style.display = "block";
+}
+
+/*
+    Closes the load confirmation form dialog.
+*/
+const closeLoadForm = () => {
+    let loadForm = document.querySelector("#loadForm");
+    loadForm.style.display = "none";
+}
+
+/*
+    Splits the provided selector data into its target and tag components.
+*/
+const splitSelector = selector => {
+    let parts = [];
+
+    let target = selector;
+    let tags = "";
+    let tagIndex = selector.indexOf("[");
+    // Verify that selector tags exist
+    if (tagIndex > 0) {
+        let selectorParts = selector.split("[");
+        target = selectorParts[0];
+
+        // Retrieve remaining selector parts
+        for (let index = 1; index < selectorParts.length; index++) {
+            let part = selectorParts[index];
+            tags += "[" + part;
+        }
+    }
+
+    parts.push(target);
+    parts.push(tags);
+
+    return parts;
+}
+
+/*
+    Attempts to load the provided command as a JSON-containing command and creates the proper JSON elements on the page.
+*/
+const loadCommand = initialCommand => {
+    let success = false;
+
+    // Empty the elements list
+    let elementsList = document.querySelector("#elements");
+    elementsList.textContent = "";
+
+    // Replace the starting forward slash if necessary
+    let command = initialCommand.replaceAll(/^\//g, "");
+
+    // Break command into managable pieces
+    let parts = command.split(" ");
+
+    if (parts.length >= 3) {
+        let commandType = parts[0];
+
+        // Load selector data
+        let selector = "";
+        let lastIndex = 1;
+        for (let index = 1; index < parts.length; index++) {
+            let part = parts[index];
+            selector += " " + part;
+            lastIndex++;
+
+            // Exit when closing bracket is provided
+            if (part.endsWith("]") || (index === 1 && !part.endsWith("]"))) {
+                break;
+            }
+        }
+        selector = selector.trim();
+        let selectorParts = splitSelector(selector);
+        let target = selectorParts[0];
+        let tags = selectorParts[1];
+
+        // Verify that the selector tags could be loaded correctly
+        if (lastIndex < parts.length) {
+
+            // Load JSON data
+            let data = "";
+            for (let index = lastIndex; index < parts.length; index++) {
+                data += " " + parts[index];
+            }
+            data = data.trim();
+
+            // Attempt to load JSON elements from data
+            let elements = [];
+            try {
+                elements = JSON.parse(data);
+            } catch {
+                return false;
+            }
+
+            // Fill generator page with data
+            let commandInput = document.querySelector("#command");
+            let targetInput = document.querySelector("#targetSelector");
+            let targetTagsInput = document.querySelector("#targetSelectorTags");
+
+            targetInput.value = target;
+            targetTagsInput.value = tags;
+            elements.forEach(element => {
+                let node;
+                let nodeId = 0;
+
+                // Load specific data based on type of JSON data
+                if (element.text) {
+                    node = addTextElement();
+                    nodeId = node.getAttribute("data-id");
+                    document.querySelector(`#elementText${nodeId}`).value = element.text;
+                } else if (element.selector) {
+                    node = addSelectorElement();
+                    nodeId = node.getAttribute("data-id");
+                    let selectorParts = splitSelector(element.selector);
+                    let target = selectorParts[0];
+                    let tags = selectorParts[1];
+                    document.querySelector(`#elementSelector${nodeId}`).value = target;
+                    document.querySelector(`#elementSelectorTags${nodeId}`).value = tags;
+                } else if (element.score) {
+                    node = addScoreElement();
+                    nodeId = node.getAttribute("data-id");
+                    let selectorParts = splitSelector(element.score.name);
+                    let target = selectorParts[0];
+                    let tags = selectorParts[1];
+                    let objective = element.score.objective;
+                    document.querySelector(`#elementSelector${nodeId}`).value = target;
+                    document.querySelector(`#elementSelectorTags${nodeId}`).value = tags;
+                    document.querySelector(`#elementObjective${nodeId}`).value = objective;
+                } else if (element.entity || element.block || element.container) {
+                    node = addNbtElement();
+                    nodeId = node.getAttribute("data-id");
+                    let type;
+                    let target = "";
+                    let tags = "";
+                    let nbt = element.nbt;
+
+                    // Load specific data
+                    if (element.entity) {
+                        type = "entity";
+                        let selectorParts = splitSelector(element.entity);
+                        target = selectorParts[0];
+                        tags = selectorParts[1];
+                    } else if (element.container) {
+                        type = "container";
+                        target = element.container;
+                    } else {
+                        type = "block";
+                        target = element.block;
+                    }
+
+                    document.querySelector(`#elementNbtType${nodeId}`).value = type;
+                    document.querySelector(`#elementSelector${nodeId}`).value = target;
+                    document.querySelector(`#elementSelectorTags${nodeId}`).value = tags;
+                    document.querySelector(`#elementNbt${nodeId}`).value = nbt;
+                } else if (element.keybind) {
+                    node = addKeybindElement();
+                    nodeId = node.getAttribute("data-id");
+                    let keybind = element.keybind;
+                    document.querySelector(`#elementKeybind${nodeId}`).value = keybind;
+                } else if (element.translate) {
+                    node = addTranslateElement();
+                    nodeId = node.getAttribute("data-id");
+                    let translate = element.translate;
+                    document.querySelector(`#elementTranslate${nodeId}`).value = translate;
+                } else {
+                    node = addTextElement();
+                    nodeId = node.getAttribute("data-id");
+                    document.querySelector(`#elementText${nodeId}`).value = element;
+                }
+
+                // Apply generic styling values if applicable
+                if (typeof(element) === "object") {
+                    if (element.color) {
+                        document.querySelector(`#elementColor${nodeId}`).value = element.color;
+                    }
+                    if (element.bold) {
+                        document.querySelector(`#elementBold${nodeId}`).checked = element.bold;
+                    }
+                    if (element.italic) {
+                        document.querySelector(`#elementItalic${nodeId}`).checked = element.italic;
+                    }
+                    if (element.underlined) {
+                        document.querySelector(`#elementUnderlined${nodeId}`).checked = element.underlined;
+                    }
+                    if (element.strikethrough) {
+                        document.querySelector(`#elementStrikethrough${nodeId}`).checked = element.strikethrough;
+                    }
+                    if (element.obfuscated) {
+                        document.querySelector(`#elementObfuscated${nodeId}`).checked = element.obfuscated;
+                    }
+                }
+            });
+
+            success = true;
+        }
+    }
+    return success;
+}
+
+/*
+    Attempts to load the entered command, if possible.
+*/
+const confirmLoad = () => {
+    let loadForm = document.querySelector("#loadForm");
+    let feedback = document.querySelector("#loadFeedback");
+    feedback.classList.remove("text-danger");
+    feedback.textContent = "";
+
+    let command = loadForm.command.value.trim();
+
+    if (command != "") {
+        let success = loadCommand(command);
+        if (!success) {
+            feedback.classList.add("text-danger");
+            feedback.textContent = "Failed to load the command. Be sure you are providing a valid command.";
+        } else {
+            closeLoadForm();
+        }
+    } else {
+        feedback.classList.add("text-danger");
+        feedback.textContent = "You must enter a valid command in order to load.";
+    }
+}
+
+/*
     Initializes elements on the webpage to enable Minecraft command generation
     features.
 */
@@ -487,16 +730,29 @@ const generateInit = () => {
     let generateButton = document.querySelector("#generate");
     if (generateButton) generateButton.addEventListener("click", generateCommand);
 
+    // Save Dialog
     let openSaveButton = document.querySelector("#openSaveForm");
     if (openSaveButton) openSaveButton.addEventListener("click", openSaveForm);
-
     let cancelSaveButton = document.querySelector("#cancelSaveForm");
     if (cancelSaveButton) cancelSaveButton.addEventListener("click", closeSaveForm);
     let closeSaveButton = document.querySelector("#closeSaveForm");
     if (closeSaveButton) closeSaveButton.addEventListener("click", closeSaveForm);
-
     let saveButton = document.querySelector("#confirmSave");
     if (saveButton) saveButton.addEventListener("click", confirmSave);
+
+    // Load Dialog
+    let openLoadButton = document.querySelector("#openLoadForm");
+    if (openLoadButton) openLoadButton.addEventListener("click", openLoadForm);
+    let cancelLoadButton = document.querySelector("#cancelLoadForm");
+    if (cancelLoadButton) cancelLoadButton.addEventListener("click", closeLoadForm);
+    let closeLoadButton = document.querySelector("#closeLoadForm");
+    if (closeLoadButton) closeLoadButton.addEventListener("click", closeLoadForm);
+    let loadButton = document.querySelector("#confirmLoad");
+    if (loadButton) loadButton.addEventListener("click", confirmLoad);
+
+    // Auto-load command if generated command exists in text area
+    let commandOutput = document.querySelector("#commandOutput");
+    if (commandOutput && commandOutput.value !== "") loadCommand(commandOutput.value);
 }
 
 window.addEventListener("load", generateInit);
