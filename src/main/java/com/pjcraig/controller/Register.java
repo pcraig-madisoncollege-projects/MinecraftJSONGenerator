07224ee6baf0e5edbf103c0d55ab488cd6538f13
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-05-09 15:16:05 -0500

    Add input validation for user registration on server side

--- src/main/java/com/pjcraig/controller/Register.java
+++ src/main/java/com/pjcraig/controller/Register.java
@@ -21,9 +21,13 @@ import java.io.IOException;
         urlPatterns = {"/register"}
 )
 public class Register extends HttpServlet {
+    public static final String SUCCESS_URL = "/registerSuccess.jsp";
+    public static final String FAILURE_URL = "/register.jsp";
     public static final String PARAMETER_EMAIL = "email";
     public static final String PARAMETER_NICKNAME = "nickname";
     public static final String PARAMETER_PASSWORD = "password";
+    public static final String PARAMETER_CONFIRM_PASSWORD = "confirmPassword";
+    public static final int MAX_PASSWORD_LENGTH = 40;
 
     /**
      * Forwards the user to the user registration page.
@@ -47,18 +51,56 @@ public class Register extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         GenericDao dao = new GenericDao(User.class);
 
-        // TODO: Implement input processing for safety and display feedback to users
+        String feedback = null;
+        String url = SUCCESS_URL;
+        boolean success = true;
+
         String email = request.getParameter(PARAMETER_EMAIL);
         String nickname = request.getParameter(PARAMETER_NICKNAME);
         String password = request.getParameter(PARAMETER_PASSWORD);
+        String confirmPassword = request.getParameter(PARAMETER_CONFIRM_PASSWORD);
 
-        User user = new User(email, password, nickname);
-        Role role = new Role("user", user);
-        user.addRole(role);
+        if (email != null && nickname != null
+                && password != null && confirmPassword != null) {
 
-        dao.insert(user);
+            // Remove whitespace characters so that the result can be compared
+            String cleanedPassword = password.replaceAll("\\s", "");
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/registerSuccess.jsp");
+            // Verify that password was passed in
+            if (password.trim().isEmpty()) {
+                feedback = "You must provide a valid password";
+                success = false;
+
+            // Password does not match confirmed password
+            } else if (!password.equals(confirmPassword)) {
+                feedback = "Your passwords do not match.";
+                success = false;
+
+            // Password contains whitespace characters
+            } else if (!password.equals(cleanedPassword)) {
+                feedback = "Your password cannot contain spaces.";
+                success = false;
+
+            // Password is too long
+            } else if (password.length() > MAX_PASSWORD_LENGTH) {
+                feedback = String.format("Your password cannot be longer than %d characters.", MAX_PASSWORD_LENGTH);
+                success = false;
+            }
+        }
+
+        // Verify that no problem occurred before creating new account
+        if (success) {
+            User user = new User(email, password, nickname);
+            Role role = new Role("user", user);
+            user.addRole(role);
+
+            dao.insert(user);
+        } else {
+            url = FAILURE_URL;
+        }
+
+        request.setAttribute("feedback", feedback);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(url);
         dispatcher.forward(request, response);
     }
 }
